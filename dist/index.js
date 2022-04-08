100bae63a12b7c34abb0fcee7d6faf4e75b08f1a
Justin Sheckler <self@jaykayess.com>
2022-04-08 11:03:19 -0400

    Improve CLI arg and URL handling

--- dist/index.js
+++ dist/index.js
@@ -2975,54 +2975,67 @@ async function run() {
     const repositoryName = core.getInput("repository_name");
     const tagName = core.getInput("tag_name");
     const tarballUrl = core.getInput("tarball_url");
-    const awsRepoUrl = core.getInput("aws_repo_url");
-    const awsCcUsername = core.getInput("aws_cc_username");
-    const awsCcPassword = core.getInput("aws_cc_password");
     const awsCcBranchname = core.getInput("aws_cc_branchname");
     const githubToken = core.getInput("github_token");
+
+    const awsRepoUrl = new URL(core.getInput("aws_repo_url"));
+    awsRepoUrl.username = core.getInput("aws_cc_username");
+    awsRepoUrl.password = core.getInput("aws_cc_password");
 
     const repoDir = "deploy_repo";
     const archiveName = `${repositoryName}-${tagName}`;
 
     core.info("Cloning AWS CC repo");
 
-    await exec.exec(
-      `git clone "https://${encodeURIComponent(
-        awsCcUsername
-      )}:${encodeURIComponent(awsCcPassword)}@${awsRepoUrl}" ${repoDir}`
-    );
+    await exec.exec("git", ["clone", awsRepoUrl.toString(), repoDir]);
 
-    await exec.exec(`git checkout -B ${awsCcBranchname}`, [], {
+    await exec.exec("git", ["checkout", "-b", awsCcBranchname], {
       cwd: repoDir,
     });
 
     core.info("Downloading release");
 
-    await exec.exec(
-      `curl -H "Authorization: token ${githubToken}" -sL "${tarballUrl}" -o ${archiveName}.tar.gz`
-    );
+    await exec.exec("curl", [
+      "-H",
+      `Authorization: token ${githubToken}`,
+      "-sL",
+      tarballUrl,
+      "-o",
+      `${archiveName}.tar.gz`,
+    ]);
 
     core.info("Unpacking release");
 
-    await exec.exec(
-      `tar -xzf ${archiveName}.tar.gz -C ${repoDir} --strip-components 1`
-    );
+    await exec.exec("tar", [
+      "-xzf",
+      `${archiveName}.tar.gz`,
+      "-C",
+      repoDir,
+      "--strip-components",
+      "1",
+    ]);
 
     core.info("Configuring AWS CC repo");
 
-    await exec.exec("git config user.email 'deployer@speedchain.com'", [], {
+    await exec.exec(
+      "git",
+      ["config", "user.email", "deployer@speedchain.com"],
+      {
+        cwd: repoDir,
+      }
+    );
+
+    await exec.exec("git", ["config", "user.name", "Deployer"], {
       cwd: repoDir,
     });
 
-    await exec.exec("git config user.name 'Deployer'", [], { cwd: repoDir });
-
     core.info("Committing changes to AWS CC repo");
 
-    await exec.exec("git add .", [], { cwd: repoDir });
+    await exec.exec("git", ["add", "."], { cwd: repoDir });
 
-    await exec.exec(`git commit -m '${tagName}'`, [], { cwd: repoDir });
+    await exec.exec("git", ["commit", "-m", tagName], { cwd: repoDir });
 
-    await exec.exec(`git push -u origin ${awsCcBranchname}`, [], {
+    await exec.exec("git", ["push", "-u", "origin", awsCcBranchname], {
       cwd: repoDir,
     });
   } catch (error) {
